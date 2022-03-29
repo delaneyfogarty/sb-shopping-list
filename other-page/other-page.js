b9a6997a3d368980390d74f3ba40f5caa0e46766
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-29 11:11:43 -0700

    form event listener

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,13 +1,52 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, deleteShoppingList, getShoppingList, buyListItem, createListItem } from '../fetch-utils.js';
+import { renderItem } from '../render-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const listEl = document.querySelector('.shopping-list');
+const listForm = document.querySelector('.shopping-list-form');
 
 logoutButton.addEventListener('click', () => {
   logout();
 });
 
 window.addEventListener('load', () => {
-    fetchAndDisplayShoppingList();
+  fetchAndDisplayShoppingList();
 });
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const data = new FormData(listForm);
+
+  await createListItem({
+    item: data.get('item'),
+    amount: data.get('amount'),
+    is_bought: false,
+  });
+
+  listForm.reset();
+
+  await fetchAndDisplayShoppingList();
+});
+
+async function fetchAndDisplayShoppingList() {
+  listEl.textContent = '';
+
+  const listData = await getShoppingList();
+
+  for (let item of listData) {
+    const itemEl = renderItem(item);
+
+    if (item.is_bought) {
+      itemEl.classlist.add('is_bought');
+    } else {
+      itemEl.addEventListener('click', async () => {
+        await buyListItem(item.id);
+        fetchAndDisplayShoppingList;
+      });
+    }
+    listEl.append(itemEl);
+  }
+
+}
